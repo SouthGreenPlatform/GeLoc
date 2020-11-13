1db30885864c933df6d1a5ba9af72fd859b6f391
Marilyne <marilyne.summo@cirad.fr>
2020-11-13 16:38:25 +0100

    fix script for domains on several CDS

--- scripts/prot2genCoord_dom_json.pl
+++ scripts/prot2genCoord_dom_json.pl
@@ -30,6 +30,8 @@ my $genPosStop;
 my $reste;
 my $foundPos = "false";
 my $foundNeg = "false";
+my $startOk = "false";
+my $stopOk = "false";
 
 #crée le fichier de sortie
 open OUT, ">$outfile" or die "cannot create $outfile !\n";
@@ -52,6 +54,8 @@ while (<INFILE>) {
 
         $foundPos = "false";
         $foundNeg = "false";
+        $startOk = "false";
+        $stopOk = "false";
 
         #parse le GFF pour récupérer les coordonées
         #parse que les positifs
@@ -69,16 +73,8 @@ while (<INFILE>) {
                 $orientation = $3;
                 $cdsid =$5;
                 $length = $stop - $start;
-                                
-                if($domStop > $length){
-                    #print "$domStop ça dépasse $length ";
-                    $domStop = $domStop - $length;
-                    #print "Il reste $domStop\n";
-                }else{
-                    $genPosStop = $domStop + $start;
-                    $domStop =0;
-                    #print "$domStop ca rentre dans $length nouvelle coordonnée : $genPosStop\n";
-                }
+                
+
                 if($domStart > $length){
                     #print "$domStart ça dépasse $length ";
                     $domStart = $domStart - $length;
@@ -88,6 +84,19 @@ while (<INFILE>) {
                     $domStart =0;
                     #print "$domStart ca rentre dans $length nouvelle coordonnée : $genPosStart\n";
                 }
+                if($domStop > $length){
+                    #print "$domStop ça dépasse $length ";
+                    $domStop = $domStop - $length;
+                    #print "Il reste $domStop\n";
+                }else{
+                    $genPosStop = $domStop + $start;
+                    $domStop =0;
+                    #print "$domStop ca rentre dans $length nouvelle coordonnée : $genPosStop\n";
+                }
+
+                #si les deux positions sont dans ce CDS 
+                #on écrit
+                #on a trouvé
                 if($genPosStart > 0 && $genPosStop > 0){
                     push @{ $outhash{$id}{$cdsid}{$domain} }, "$genPosStart;$genPosStop";
                     #print OUT "$id;$domain;$genPosStart;$genPosStop\n";
@@ -95,6 +104,34 @@ while (<INFILE>) {
                     $genPosStart =0;
                     $genPosStop =0;
                     last;
+
+                #si on a que la position start
+                #alors le domaine s'étend sur un autre CDS
+                #on écrit le domaine de genPosStart jusqu'à la fin du CDS
+                }elsif($genPosStart > 0){
+                    push @{ $outhash{$id}{$cdsid}{$domain} }, "$genPosStart;$stop";
+                    $genPosStart =0;
+                    $startOk = "true";
+                    next;
+
+                #si on a déjà trouvé le start mais toujours pas le stop
+                #alors le domaine couvre le CDS entier
+                #On ecrit le le domaine sur les start et stop du CDS
+                }elsif($startOk eq "true" && $genPosStop == 0){
+                    push @{ $outhash{$id}{$cdsid}{$domain} }, "$start;$stop";
+                    next;
+
+                #si on a que la position stop
+                #alors le domaine se termine sur ce CDS
+                #on écrit le domaine du début du CDS jusqu'à la position genPosStop
+                #on a trouvé
+                }elsif($startOk eq "true" && $genPosStop > 0){
+                    push @{ $outhash{$id}{$cdsid}{$domain} }, "$start;$genPosStop";
+                    $foundPos = "true";
+                    $stopOk = "true";
+                    $genPosStop =0;
+                    last;
+
                 }else{
                     $foundPos = "false";
                     next;
@@ -117,15 +154,6 @@ while (<INFILE>) {
                     $cdsid =$5;
                     $length = $stop - $start;
 
-                    if($domStop > $length){
-                        #print "$domStop ça dépasse $length ";
-                        $domStop = $domStop - $length;
-                        #print "Il reste $domStop\n";
-                    }else{
-                        $genPosStop = $stop - $domStop;
-                        $domStop =0;
-                        #print "$domStop ca rentre dans $length nouvelle coordonnée : $genPosStop\n";
-                    }
                     if($domStart > $length){
                         #print "$domStart ça dépasse $length ";
                         $domStart = $domStart - $length;
@@ -135,6 +163,20 @@ while (<INFILE>) {
                         $domStart = 0;
                         #print "$domStart ca rentre dans $length nouvelle coordonnée : $genPosStart\n";
                     }
+
+                    if($domStop > $length){
+                        #print "$domStop ça dépasse $length ";
+                        $domStop = $domStop - $length;
+                        #print "Il reste $domStop\n";
+                    }else{
+                        $genPosStop = $stop - $domStop;
+                        $domStop =0;
+                        #print "$domStop ca rentre dans $length nouvelle coordonnée : $genPosStop\n";
+                    }
+                    
+                    #si les deux positions sont dans ce CDS 
+                    #on écrit
+                    #on a trouvé
                     if($genPosStart > 0 && $genPosStop > 0){
                         #inverse le positions pour les negatifs
                         push @{ $outhash{$id}{$cdsid}{$domain} }, "$genPosStop;$genPosStart";
@@ -143,6 +185,34 @@ while (<INFILE>) {
                         $genPosStop =0;
                         $foundNeg = "true";
                         last;
+                    
+                    #si on a que la position start
+                    #alors le domaine s'étend sur un autre CDS
+                    #on écrit le domaine de genPosStart jusqu'au début du CDS
+                    }elsif($genPosStart > 0){
+                        push @{ $outhash{$id}{$cdsid}{$domain} }, "$start;$genPosStart";
+                        $genPosStart =0;
+                        $foundNeg = "false";
+                        $startOk = "true";
+                        next;
+                    
+                    #si on a déjà trouvé le start mais toujours pas le stop
+                    #alors le domaine couvre le CDS entier
+                    #On ecrit le le domaine sur les start et stop du CDS
+                    }elsif($startOk eq "true" && $genPosStop == 0){
+                        push @{ $outhash{$id}{$cdsid}{$domain} }, "$start;$stop";                        
+                        next;
+
+                    #si on a que la position stop
+                    #alors le domaine se termine sur ce CDS
+                    #on écrit le domaine de la fin du CDS jusqu'à la position genPosStop 
+                    #on a trouvé
+                    }elsif($genPosStop > 0){
+                        push @{ $outhash{$id}{$cdsid}{$domain} }, "$genPosStop;$stop";
+                        $foundNeg = "true";
+                        $genPosStop =0;
+                        last;
+
                     }else{
                         $foundNeg = "false";
                         next;
